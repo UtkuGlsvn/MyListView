fdc358401ff087c608b64c1794dcb36e14d6bdf0
utkuglsvn <utku.glsvn@gmail.com>
2019-01-11 02:06:15 +0300

    List click

--- app/src/main/java/com/example/glsvn/listview/MainActivity.java
+++ app/src/main/java/com/example/glsvn/listview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.glsvn.listview;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         CustomAdapter adapter = new CustomAdapter(MainActivity.this,item);
         listView=findViewById(R.id.listview);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Dialog builder = new Dialog(MainActivity.this);
+                builder.setTitle("İnfo:"+position);
+                builder.setCancelable(true);
+                builder.show();
+            }
+        });
     }
 
 }
